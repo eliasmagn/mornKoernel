b065a81c7ad4c119a691bb89c84273345f315573
Erwan <erwanleboucher@gmail.com>
2017-06-13 19:18:10 +0200

    Revert "qpnp-haptic: underp"
    This reverts commit 40d79d388fe753f78876ce7de51920e9184983d2.

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -1861,7 +1861,7 @@ static void qpnp_hap_worker(struct work_struct *work)
 		qpnp_hap_set(hap, hap->state);
 	}
 
-	if (hap->vcc_pon && !reg_en ) {
+	if (hap->vcc_pon && !reg_en &&(hap->state == 0)) {
 		rc = regulator_disable(hap->vcc_pon);
 		if (rc)
 			pr_err("%s: could not disable vcc_pon regulator\n",
